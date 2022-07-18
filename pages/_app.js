997238de2b791f05265789a094d752502b8c7079
Muhammad Mubashshir Ali <jssmithsa6516@gmail.com>
2022-07-18 15:07:26 +0530

    Cart state props exported

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,71 @@ import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  const [Cart, setCart] = useState('')
+  const [Cart, setCart] = useState({})
+  const [subTotal, setsubTotal] = useState(0)
+
+  useEffect(() => {
+    try {
+      if(localStorage.getItem('cart')){
+        setCart(JSON.parse(localStorage.getItem('cart')))
+      }
+    } catch (err) {
+      console.error(err)
+      localStorage.clear()
+    }
+  }, [])
+  
+
+  const saveCart = (newCart) =>{
+    localStorage.setItem("cart",newCart)
+    let subt = 0;
+    let keys = Object.keys(Cart)
+    for (let i = 0; i < keys.length; i++) {
+      subt += Cart[keys[i]].price *  Cart[keys[i]].qty
+    }
+    setsubTotal(subt)
+  }
+
+  const clearCart = () => {
+    setCart({})
+    saveCart({})
+    alert('Cart has been cleared')
+  }
+  
+  const addToCart = (productId,qty,price,name,size,variant) => {
+    let myCart = Cart
+    if (productId in Cart){
+      myCart[productId].qty = Cart[productId].qty + qty;
+    }
+    else {
+      myCart[productId] = {qty:1,price,name,size,variant}
+    }
+    setCart(myCart)
+    saveCart(Cart)
+  }
+
+  const removeFromCart = (productId,qty,price,name,size,variant) => {
+    let myCart = Cart
+    if (productId in Cart){
+      myCart[productId].qty = Cart[productId].qty - qty;
+    }
+    if(myCart[productId].qty <= 0) {
+      delete myCart[productId]
+    }
+    setCart(myCart)
+    saveCart(Cart)
+  }
   return (<>
     {/* <HeadComp/> */}
-    <Navbar />
+    <Navbar Cart={Cart} addToCart={addToCart} removeFromCart={removeFromCart} saveCart={saveCart} setCart={setCart} clearCart={clearCart} subTotal={subTotal} />
     <div>
       <div className='min-h-screen'>
-      <Component {...pageProps} />
+      <Component Cart={Cart} addToCart={addToCart} removeFromCart={removeFromCart} saveCart={saveCart} setCart={setCart} clearCart={clearCart} subTotal={subTotal} {...pageProps} />
       </div>
       <Footer />
     </div>
